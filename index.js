6bbe46afde516f091fe66752ae163e27feba82a1
Felipe Machado <feelipe@email.com>
2019-06-03 16:24:04 -0300

    Criamos a nossa class User

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
 function addLine(dataUser) {
+  console.log(dataUser);
 
-  var tr = document.createElement("tr");
-
-  tr.innerHTML = `<tr>
+  document.getElementById("table-users").innerHTML = `<tr>
 <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
 <td>${dataUser.name}</td>
 <td>${dataUser.email}</td>
@@ -16,26 +15,33 @@ function addLine(dataUser) {
   <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
 </td>
 </tr>`;
-
-  document.getElementById("table-users").appendChild(tr);
-
 }
 
-document.getElementById("form-user-create").addEventListener("submit", function (event) {
-  event.preventDefault();
+document
+  .getElementById("form-user-create")
+  .addEventListener("submit", function(event) {
+    event.preventDefault();
 
-  fields.forEach(function (field, index) {
-
-    if (field.name == "gender") {
-
-      if (field.checked) {
+    fields.forEach(function(field, index) {
+      if (field.name == "gender") {
+        if (field.checked) {
+          user[field.name] = field.value;
+        }
+      } else {
         user[field.name] = field.value;
       }
+    });
 
-    } else {
-      user[field.name] = field.value;
-    }
+    const objectUser = new User(
+      user.name,
+      user.gender,
+      user.birth,
+      user.country,
+      user.email,
+      user.password,
+      user.photo,
+      user.admin
+    );
 
+    addLine(objectUser);
   });
-  addLine(user);
-});
